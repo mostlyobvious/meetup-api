d93b30a42c42b278ba99b15a788c5a7b92e80413
Paweł Pacana <pawel.pacana@gmail.com>
2013-08-01 12:05:41 +0200

    Little helper goes a long way.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::API
 
   extend Dependor::Injectable
   inject_from Injector
+
+  def render_json_api(resource)
+    respond_to do |format|
+      format.json_api { render json_api: resource }
+    end
+  end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,18 +2,14 @@ class EventsController < ApplicationController
   inject :event_repository, :event_serializer, :event_collection_serializer
 
   def index
-    events = event_repository.events_for_public_listing
-
-    respond_to do |format|
-      format.json_api { render json_api: event_collection_serializer.serialize(events) }
-    end
+    events   = event_repository.events_for_public_listing
+    resource = event_collection_serializer.serialize(events)
+    render_json_api(resource)
   end
 
   def show
-    event = event_repository.find_event_by_id(params[:id])
-
-    respond_to do |format|
-      format.json_api { render json_api: event_serializer.serialize(event) }
-    end
+    event    = event_repository.find_event_by_id(params[:id])
+    resource = event_serializer.serialize(event)
+    render_json_api(resource)
   end
 end
