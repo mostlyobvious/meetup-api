b48d7e68fc09d97a5ee236840666b90b18098b9f
Paweł Pacana <pawel.pacana@gmail.com>
2013-08-01 11:05:55 +0200

    Less confusing specs with parsed vs. serialized wording.

--- spec/serializers/event_collection_serializer_spec.rb
+++ spec/serializers/event_collection_serializer_spec.rb
@@ -5,8 +5,9 @@ describe EventCollectionSerializer do
   let(:event_serializer)            { fake(:event_serializer, serialize: '{}') }
   let(:event)                       { fake(:event, name: 'bazinga') }
   let(:event_collection)            { [event, event] }
-  let(:serialized_event_collection) { JSON.parse(event_collection_serializer.serialize(event_collection)) }
+  let(:serialized_event_collection) { event_collection_serializer.serialize(event_collection) }
+  let(:parsed_event_collection)     { JSON.load(serialized_event_collection) }
 
   specify { expect(event_collection_serializer).to respond_to(:serialize) }
-  specify { expect(serialized_event_collection).to have(2).items }
+  specify { expect(parsed_event_collection).to have(2).items }
 end
--- spec/serializers/event_serializer_spec.rb
+++ spec/serializers/event_serializer_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe EventSerializer do
   let(:event_serializer) { isolate(described_class) }
-  let(:serialized_event) { JSON.parse(event_serializer.serialize(event)) }
+  let(:serialized_event) { event_serializer.serialize(event) }
+  let(:parsed_event)     { JSON.load(serialized_event) }
   let(:event)            { fake(:event, name: 'bazinga') }
 
   specify { expect(event_serializer).to respond_to(:serialize) }
-  specify { expect(serialized_event['name']).to eq('bazinga') }
+  specify { expect(parsed_event['name']).to eq('bazinga') }
 end
